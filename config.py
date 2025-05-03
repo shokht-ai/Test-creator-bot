7a84e5987e1e90c40153191b59b25144a5c5f69a
shokht-ai <shokht2007@gmail.com>
2025-05-03 21:05:39 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = "7349199816:AAF1fKZnGWpv0TRhWgcYCFt6ePu6Y5oTHIY"
+BOT_TOKEN = "YOUR_BOT_TOKEN"
 WEBHOOK_PATH = "/webhook"
 WEBHOOK_SECRET = "your_custom_secret_token"
 BASE_WEBHOOK_URL = "https://neoshokh.pythonanywhere.com/"
