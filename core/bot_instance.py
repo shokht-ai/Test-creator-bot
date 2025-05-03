3876e946cbebdf638b2c07329894ef457564df43
shokht-ai <shokht2007@gmail.com>
2025-05-03 21:07:43 +0500

    Update bot_instance.py

--- core/bot_instance.py
+++ core/bot_instance.py
@@ -3,6 +3,6 @@ from aiogram import Bot
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
-API_TOKEN = "7349199816:AAGlN739zzmn-Ny80T8GWBZz8NMqqG_Xejw"
+API_TOKEN = "YOUR_BOT_TOKEN"
 
 bot = Bot(token=API_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
